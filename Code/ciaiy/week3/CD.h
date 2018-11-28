6428a4f967e9a21557e3fd0e1d8dd40b733fbc42
ciaiy <1374390675@qq.com>
2018-11-28 18:30:41 +0800

    init carry's workspace and some code

---
+++ Code/ciaiy/week3/CD.h
@@ -0,0 +1,18 @@
+#ifndef CD_H_
+#define CD_H_
+
+class C {
+    public :
+        C(int a, int b);
+        C();
+        ~C();
+    private :
+        int a;
+        int b;
+};
+
+class D : public C {
+    
+}
+
+#endif
